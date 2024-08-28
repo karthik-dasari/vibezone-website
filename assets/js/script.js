8797234f33d74d9eadf981f1a39c27f749940ac2
karthik-dasari <karthik.dasari2001@gmail.com>
2024-08-29 02:23:53 +0530

    cp

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,3 +119,54 @@ addEventOnElements(hoverElements, "mouseover", function () {
 addEventOnElements(hoverElements, "mouseout", function () {
   cursor.classList.remove("hovered");
 });
+
+new Splide( '#image-carousel', {
+  perPage    : 3,
+  breakpoints: {
+    640: {
+      perPage: 1,
+    },
+  },
+  pagination: false,
+  gap        : '4rem',
+  focus      : 'center',
+  // type       : 'loop',
+  autoplay   : true,
+  interval   : 3000,
+  pauseOnHover: true,
+  pauseOnFocus: true,
+  arrows     : false,
+} ).mount();
+
+document.getElementById('submit').addEventListener('click', function (e) {
+  e.preventDefault();
+  const form = document.querySelector('form');
+  const name = document.querySelector('input[name="your-name"]').value;
+  const email = document.querySelector('input[name="your-email"]').value;
+  const message = document.querySelector('textarea[name="your-message"]').value;
+  const subject = document.querySelector('input[name="your-subject"]').value;
+  var data = {
+    'name': name,
+    'email': email,
+    'message': message,
+    'subject': subject
+  }
+  
+  
+  fetch('https://script.google.com/macros/s/AKfycby6SI2LX3Qno6BPtKOv7Nu0QdhkUaT78Dse_NhMhhq52TT-_Uo7Ru9mMc9ZuKX_go_O/exec', {
+    method: 'POST',
+    data: data,
+    dataType: 'json',
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      form.reset();
+      alert('Thanks for your message!');
+    } else {
+      alert('Oops! There was a problem submitting your form');
+    }
+  });
+}
+);
